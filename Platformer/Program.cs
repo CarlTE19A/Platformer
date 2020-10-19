5cca82a3ae0b48d3e5d03675339144515f407066
Carl <nordholm.carl@gmail.com>
2020-10-19 21:32:26 +0200

    10-19.2

--- Platformer/Program.cs
+++ Platformer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Raylib_cs;
 
 namespace Platformer
@@ -14,9 +15,22 @@ namespace Platformer
             int playerY = height/2-size/2;
             int worldX = 0;
             int worldY = 0;
+            bool fall;
+
+            int platformLength = 2; //How many platforms integrated
+
+            int[] platformX = new int[platformLength];
+            int[] platformY = new int[platformLength];
+            int[] platformWidth = new int[platformLength];
+            int[] platformHeight = new int[platformLength];
+            platformX[0] = -500; platformY[0] = 500; platformWidth[0] = 2000; platformHeight[0] = 25; 
+            platformX[1] = -200; platformY[1] = 300; platformWidth[1] = 200; platformHeight[1] = 25; 
+            int jump = 0;
+
             Raylib.InitWindow(width,height, "Platformer");
             while(Raylib.WindowShouldClose() !=  true) //Reapeats every Frame
             {
+                fall = true;
                 if(Raylib.IsKeyDown(KeyboardKey.KEY_A) || Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
                 {
                     if(width/2 - 100 < playerX)
@@ -25,9 +39,10 @@ namespace Platformer
                     }
                     else
                     {
-                        worldX--;
+                        worldX++;
                     }
                 }
+                
                 if(Raylib.IsKeyDown(KeyboardKey.KEY_D) || Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
                 {
                     if(width/2 + 100 > playerX)
@@ -36,17 +51,40 @@ namespace Platformer
                     }
                     else
                     {
-                        worldX++;
+                        worldX--;
                     }
                 }
                 
+                for(int i = 0; i < platformX.Length; i++)
+                {
+                    if(playerY+size == platformY[i] && playerX > platformX[i] && playerX + size < platformX[i] + platformWidth[i])
+                    {
+                        fall = false;
+                    }
+                    if(Raylib.IsKeyPressed(KeyboardKey.KEY_W) || Raylib.IsKeyPressed(KeyboardKey.KEY_UP))
+                    {
+                        jump = 40;
+                    }
+                    
+                }
+                if(fall == true)
+                {
+                    playerY++;
+                }
+                if(jump != 0)
+                {
+                playerY -= jump;
+                    jump-=2;
+                }
 
                 //Graphics
                 Raylib.BeginDrawing(); //Begin Draw
                 Raylib.ClearBackground(Color.BLACK); //Background
                 Raylib.DrawRectangle(playerX, playerY, size, size, Color.GREEN); //Player
-                Raylib.DrawRectangle(worldX + 50, worldY + 400,500,25, Color.WHITE);
-                
+                for(int i = 0; i < platformX.Length; i++)
+                {
+                    Raylib.DrawRectangle(worldX + platformX[i], worldY + platformY[i], platformWidth[i], platformHeight[i], Color.WHITE);
+                }
 
                 Raylib.EndDrawing(); //End Draw
             }
